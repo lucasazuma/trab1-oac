f829b72294737e32bb2efa115b051b63f62365f6
lucasazuma <azumalucas@gmail.com>
2021-08-08 22:33:22 -0300

    compiling instructions

--- binary.c
+++ binary.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 #include "validations.h"
 #include "binary.h"
+#define STANDARD 0xFF
 
-int32_t extraction_mask(uint8_t byteIndexInInt) {
-    int32_t mask = 0xFF << (byteIndexInInt * 8);
-
-    return mask;
+int32_t extraction_mask(uint8_t byte_index) {
+    return STANDARD << (byte_index * 8);
 }
 
-int32_t int_from_byte(int32_t fullInt, int32_t mask) {
-    return mask & fullInt;
+int32_t int_from_byte(int32_t memory_data, int32_t mask) {
+    return mask & memory_data;
 }
 
 
--- binary.h
+++ binary.h
@@ -2,8 +2,8 @@
 #ifndef OAC_BINARY_H
 #define OAC_BINARY_H
 
-int32_t extraction_mask(uint8_t byteIndexInInt);
+int32_t extraction_mask(uint8_t byte_index);
 
-int32_t int_from_byte(int32_t fullInt, int32_t mask);
+int32_t int_from_byte(int32_t memory_data, int32_t mask);
 
 #endif //OAC_BINARY_H
